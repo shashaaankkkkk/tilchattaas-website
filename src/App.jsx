a608a25ca672b05eeb4f57eca3b4a85372c79a57
Shashank Shekhar <shashankshekhar8534@gmail.com>
2024-11-09 02:05:29 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Home from './components/Home'
+import Brands from './components/Logo'
+import Integration from './components/Integrations'
+import Features from './components/Features'
+import HowItWorks from './components/Works'
+import Testi from './components/Testi'
+import Action from './components/Action'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+   <Home/>
+   <Brands></Brands>
+   <Integration></Integration>
+   <HowItWorks/>
+    <Testi/>
+    <Action/>
     </>
   )
 }
